ebb1728bb16d447fa2a76e925d4e2142936b33e3
Ashish Agarwal <ashishag07@gmail.com>
2024-12-27 16:59:31 +0530

    index.js file is modified: import redux provider. userSlice file is created.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import AuthProvider from "./components/context/authContext";
 
+import { Provider } from "react-redux";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
